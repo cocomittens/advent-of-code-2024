ad2140c7ebfb6dcd017e63c11e5714b517628fcd
Corrie Stoddard <corriestoddard@gmail.com>
2024-12-01 12:05:53 -0800

    Complete day 1

--- day1.py
+++ day1.py
@@ -7,17 +7,21 @@ right = []
 # process data into pairs
 def process_data():
     data_array = data.split("\n")
+
     for i, row in enumerate(data_array):
         pair = list(map(int, row.split("   ")))
-        heapq.heappush(left, pair[0])
-        heapq.heappush(right, pair[1])
-    return left, right
+        left.append(pair[0])
+        right.append(pair[1])
+
+    heapq.heapify(left)
+    heapq.heapify(right)
 
 def calculate_distance(num1, num2):
     return abs(num1 - num2)
 
 def add_distances():
     sum = 0
+
     for i in range(len(left)):
         left_item = heapq.heappop(left)
         right_item = heapq.heappop(right)
@@ -27,26 +31,31 @@ def add_distances():
 
 def calculate_frequency():
     freq = {}
+
     for i, num in enumerate(right):
         if num in freq:
             freq[num] += 1
         else:
             freq[num] = 1
+
     return freq
 
 def calculate_similarity(freq):
     similarity = 0
+
     for num in left:
         if num in freq:
             similarity += num * freq[num]
+
     return similarity
 
 if __name__ == '__main__':
     process_data()
-    # Part 1
-    result = add_distances()
-    print(result)
-    # Part 2
     frequency = calculate_frequency()
     similarity = calculate_similarity(frequency)
+    distance_sum = add_distances()
+
+    # Part 1
+    print(distance_sum)
+    # Part 2
     print(similarity)
