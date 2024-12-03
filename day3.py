ad0318a667afef70940e497483f9a050117cde50
Corrie Stoddard <corriestoddard@gmail.com>
2024-12-03 15:47:29 -0800

    Complete day 2

--- day3.py
+++ day3.py
@@ -1,7 +1,23 @@
 from data.day3 import data
+import re
 
 def process_data():
-    pass
+    result = re.findall("mul\(\d+,\d+\)", data)
+    return result
+
+def parse_nums(line):
+    nums = re.findall("\d+", line)
+    return list(map(int, nums))
+
+def multiply_nums(nums):
+    return nums[0] * nums[1]
 
 if __name__ == '__main__':
-    process_data()
+    data = process_data()
+    # Part 1
+    sum = 0
+    for line in data:
+        nums = parse_nums(line)
+        product = multiply_nums(nums)
+        sum += product
+    print(sum)
